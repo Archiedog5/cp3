49049e81c81245548beadfa20e90bea06be92ccf
Archiedog5 <jonathan.hatch@ucas-edu.net>
2024-10-02 04:04:57 +0000

    devolped the first tree roomes

--- wierd al.py
+++ wierd al.py
@@ -39,7 +39,7 @@ def fightp2():
     global herodp
     global p2dp
     dmg=random.randint(1,herodp)
-    dmgr=int(input("Guess a number 1-5 for your dmg: "))
+    dmgr=int(input("Guess a number 1-10 for your dmg: "))
     given = abs(dmg-dmgr)
     p2hp-=given
     print("Enimies stats are now Health:",p2hp,"Damage:",p2dp)
@@ -242,7 +242,7 @@ elif room1 == "Y":
 hallwayLorR = input("Now you are here do you want to go to left or right?(1 for left, 2 for right): ")
 if hallwayLorR ==  "2" and ifkey == "Yes":
      pass 
-elif hallwayLorR == "2" and ifkey == "Yes":
+elif hallwayLorR == "2" and ifkey == "No":
     print("This door is locked, try the other one.")
 elif hallwayLorR == "1":
     room123 = input("You see three rooms, left right and center wich one do you want to go to?(1,2 and 3,): ")
@@ -251,4 +251,22 @@ elif hallwayLorR == "1":
         print("Defence works by gussing how much thier going to damage you. Then it gets the absolute value of thier actul damage minus your guess.")
         print("Offence works the same but your gussing your dmg.")
         comabtP1()
-        print("You scavange hte nocked out gaurd and you find your secound piece of paper. And it says'")
+        pieces+=1
+        print("You scavange the nocked out gaurd and you find your secound piece of paper. And it says'941'. The same guard also has a piece of paper that says 'big iron on his hip/ Big iron on his hip/ In this town there lived an outlaw by the name of Texas Red/ Many men had tried to take him and that many men were dead/ He was vicious and a killer though a youth of 24/ And the notches")
+    elif rooom123 == "2":
+        print("You found another piece of paper and it says 'hip/ Big iron on his hip/ It was early in the morning when he rode into the town/ He came riding from the south side slowly lookin' all around/ He's an outlaw loose and running, came the whisper from each lip/ And he's here to do some business with the' ")
+        pieces+=1
+    elif room123 == "1":
+        print("You see a chest in the center of the room. But it requires a code. What is the code?;")
+        chestcode=int("Whats the code to the chest?: ")
+        if chestcode==941:
+            print("You find two things a key too the right hallway and a potion that increases your dmg by 10 points")
+             ifkey="yes"
+        else:
+            leave=0
+            while chestcode!=941 or leave=="1" :
+                print("wrong")
+                leave=input("Do you want to leave or try again. 1 for leave and 2 to tryagian.")
+
+            
+    
