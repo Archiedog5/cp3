6378dcc9b4e64b05585618583faec208a3ce12b6
Archiedog5 <jonathan.hatch@ucas-edu.net>
2024-11-15 11:50:11 -0700

    dessert

--- Dessert_shop.py
+++ Dessert_shop.py
@@ -1,28 +1,60 @@
+from abc import ABC, abstractmethod 
 
-class DesserItems:
+class DesserItems(ABC):
     def __init__(self,name):
         self.name = name
+        self.tax_precent=0.75
+
     def __str__(self):
         return f"{self.name}"
+    
+    @abstractmethod
+    def calculate_cost(self):
+        return 0
+
+    @abstractmethod
+    def calculate_tax(self):
+        return 0
+
 class Candy(DesserItems):
+
     def __init__(self, name, candy_weight, price_per_pound):
         super().__init__(name)
         self.candy_weight=candy_weight
         self.price_per_pound=price_per_pound
+
+    def calculate_cost(self, candy_weight, price_per_pound):
+        cost = candy_weight*price_per_pound
+        return cost
+    
 class Cookie(DesserItems):
+
     def __init__(self, name, cookie_quantity, price_per_dozen):
         super().__init__(name)
         self.cookie_quantity=cookie_quantity
         self.price_per_dozen=price_per_dozen
+    
+    def calculate_cost(self, cookie_quantity, price_per_dozen):
+        cookie_dozen = cookie_quantity/12
+        cost = cookie_dozen*price_per_dozen
+        return cost
+
 class IceCream(DesserItems):
     def __init__(self, name, scoop_count, price_per_scoop):
         super().__init__(name)
         self.scoop_count=scoop_count
         self.price_per_scoop=price_per_scoop
+
+    def calculate_cost(self, scoop_count, price_per_scoop):
+        cost = scoop_count*price_per_scoop
+        return cost
+
 class Sundae(IceCream):
     def __init__(self, name, scoop_count, price_per_scoop, topping_name, topping_price):
         super().__init__(name, scoop_count, price_per_scoop)
         self.topping_name=topping_name
         self.topping_price=topping_price
 
-
+    def calculate_cost(self, candy_weight, price_per_pound):
+        cost = candy_weight*price_per_pound
+        return cost
